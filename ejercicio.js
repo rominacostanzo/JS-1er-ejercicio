0b8981cda3ce7008e8a017602b444e38aa7e9074
rominacostanzo <65253721+rominacostanzo@users.noreply.github.com>
2022-10-03 18:10:19 -0300

    1er ejerc JS

---
+++ ejercicio.js
@@ -0,0 +1,17 @@
+function sumarNumeros (numero1, numero2, numero3, numero4, numero5){
+    return alert(`Su resultado es ${numero1+numero2+numero3+numero4+numero5} `); 
+        }
+
+
+
+
+sumarNumeros 
+(parseInt(prompt ("Ingrese el 1er número")), 
+parseInt (prompt ("Ingrese el 2do número")),
+parseInt (prompt ("Ingrese el 3er número")), 
+parseInt (prompt ("Ingrese el 4to número")), 
+parseInt (prompt ("Ingrese el 5to número"))
+)
+
+
+
